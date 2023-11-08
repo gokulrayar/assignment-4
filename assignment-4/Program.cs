3f342d0f57784d64737827551b4424f4006c6897
gokulrayar <gokulrayar>
2023-11-08 17:29:27 +0530

    assignment4

---
+++ assignment-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero.");
+            }
+
+            string parity = (number % 2 == 0) ? "even" : "odd";
+            Console.WriteLine($"The number is {parity}.");
+        }
+    }
+}
